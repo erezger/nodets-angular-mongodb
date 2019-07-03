606dc0d6f337672b03b48c39927abe9bdd291f98
erezger <erezger1987@gmail.com>
2019-07-03 20:55:27 +0300

    init

--- server/lib/db/mongoose.ts
+++ server/lib/db/mongoose.ts
@@ -4,8 +4,8 @@ export class MongooseDb {
 
   public connect() {
     // mongoose.connect('mongodb+srv://erez:Bereshit@cluster0-utea1.mongodb.net/test?retryWrites=true&w=majority', {
-    // mongoose.connect('mongodb+srv://erez:user123456@cluster0-utea1.mongodb.net/test?retryWrites=true&w=majority', {
-    mongoose.connect('mongodb://localhost:27017/localdb', {
+    mongoose.connect('mongodb+srv://user:user123456@cluster0-utea1.mongodb.net/test?retryWrites=true&w=majority', {
+    // mongoose.connect('mongodb://localhost:27017/localdb', {
       useNewUrlParser: true,
       useCreateIndex: true
     }).then(() => {
